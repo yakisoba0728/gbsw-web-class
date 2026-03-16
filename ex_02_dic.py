cbda063c6def74daf0a5360ce3084854ae5b4b7a
yakisoba0728 <yakihyuk0728@gmail.com>
2026-03-16 16:01:07 +0900

    Delete ex_02_dic.py

--- ex_02_dic.py
+++
@@ -1,8 +0,0 @@
-dic = {'name': 'kimdonghyeok', 'age': 16, 'gender': 'male'}
-print(dic['name'])
-dic['helloworld'] = 'true'
-print(dic)
-del dic['name']
-del dic['helloworld']
-print(dic)
-print(dic.keys())
