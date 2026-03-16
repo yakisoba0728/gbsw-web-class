5f22c64ae027fb30fc5742e66ff5a3c9160db285
yakisoba0728 <yakihyuk0728@gmail.com>
2026-03-16 16:01:06 +0900

    Create ex_02_dic.py

---
+++ ch_03/ex_02_dic.py
@@ -0,0 +1,9 @@
+dic = {'name': 'kimdonghyeok', 'age': 16, 'gender': 'male'}
+print(dic['name'])
+dic['helloworld'] = 'true'
+print(dic)
+del dic['name']
+del dic['helloworld']
+print(dic)
+print(list(dic.items()))
+print('age' in dic)
