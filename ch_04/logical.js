df8dd0fc2f9e45ae950472188b854dfeb8e7ed54
yakisoba0728 <yakihyuk0728@gmail.com>
2026-03-19 15:24:50 +0900

    Update logical.js

--- ch_04/logical.js
+++ ch_04/logical.js
@@ -14,3 +14,14 @@ console.log(!false);
 const age = 27;
 const result = age >= 8 && age <= 19 ? "팩트" : "거짓말";
 console.log(result);
+
+const nickname = null;
+const babo = nickname ?? "익명이래요";
+console.log(babo);
+
+let nick = null;
+console.log(nick ?? "익명");
+nick = undefined;
+console.log(nick ?? "익명");
+nick = "히히";
+console.log(nick ?? "익명");
