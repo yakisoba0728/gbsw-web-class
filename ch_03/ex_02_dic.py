cd74272e6ff3e843bfeea81444ba085a9c922a66
yakisoba0728 <yakihyuk0728@gmail.com>
2026-03-19 10:47:45 +0900

    remove wrong files

--- ch_03/ex_02_dic.py
+++
@@ -1,9 +0,0 @@
-dic = {'name': 'kimdonghyeok', 'age': 16, 'gender': 'male'}
-print(dic['name'])
-dic['helloworld'] = 'true'
-print(dic)
-del dic['name']
-del dic['helloworld']
-print(dic)
-print(list(dic.items()))
-print('age' in dic)
--- ch_04/ex_03.py
+++
@@ -1,8 +0,0 @@
-a, b = ('aa', 'bb')
-print(a, b)
-(a, b) = 'hihi', 'hehe'
-print(a, b)
-[a, b] = ['pepe', 'pimpi']
-print(a, b)
-a, b = b, a
-print(a, b)
